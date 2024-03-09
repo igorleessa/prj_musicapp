26de3c1d5fca5437b14464ce9bbe86f52294d5c1
Igor Lessa Robaina de Souza <igorleessa@gmail.com>
2024-03-09 10:29:20 -0300

    .

---
+++ SpotifyLike.Repository/Repository/UsuarioRepository.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SpotifyLike.Domain.Conta.Agreggates;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpotifyLike.Repository.Repository
+{
+    public class UsuarioRepository : RepositoryBase<Usuario>
+    {
+        public SpotifyLikeContext Context { get; set; }
+
+        public UsuarioRepository(SpotifyLikeContext context) : base(context)
+        {
+            Context = context;
+        }
+
+        //public Usuario GetById(Guid id)
+        //{
+        //    return this.Context.Usuarios
+        //               .Include(x => x.Assinaturas) //Caso não esteja usando lazy loading
+        //               .Include(x => x.Playlists)
+        //               .Include(x => x.Notificacoes)
+        //               //.AsSplitQuery() //Quebra a consulta por cada tipo
+        //               .FirstOrDefault(x => x.Id == id);
+        //}
+
+
+    }
+}
